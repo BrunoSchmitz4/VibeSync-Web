58f7f2ca745b610b792164f55ed9e62ef4804e6c
Bruno_Ss <brunosch250@gmail.com>
2025-06-14 19:58:16 -0300

    Update

--- src/services/spotifyConfig.js
+++ src/services/spotifyConfig.js
@@ -1,4 +1,6 @@
-const CLIENT_ID = "1832bd3ad84448e092409e382d6d47ed";
+import getClientIdValue from '../../authInfo';
+
+const CLIENT_ID = getClientIdValue;
 const REDIRECT_URI = "http://127.0.0.1:3000";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
